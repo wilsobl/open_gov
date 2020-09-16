58dad97039dcc9a334513eaec5e72860119eb078
gordie h <gordon.hammond@sling.com>
2020-09-15 20:11:07 -0600

    removed react tutorial code

--- main.go
+++ main.go
@@ -281,48 +281,6 @@ func getPemCert(token *jwt.Token) (string, error) {
 	return cert, nil
 }
 
-// JokeHandler returns a list of jokes available (in memory)
-func JokeHandler(c *gin.Context) {
-	c.Header("Content-Type", "application/json")
-
-	c.JSON(http.StatusOK, jokes)
-}
-
-// LikeJoke increments the likes of a particular joke Item
-func LikeJoke(c *gin.Context) {
-	// Check joke ID is valid
-	if jokeid, err := strconv.Atoi(c.Param("jokeID")); err == nil {
-		// find joke and increment likes
-		for i := 0; i < len(jokes); i++ {
-			if jokes[i].ID == jokeid {
-				jokes[i].Likes = jokes[i].Likes + 1
-			}
-		}
-		c.JSON(http.StatusOK, &jokes)
-	} else {
-		// the jokes ID is invalid
-		c.AbortWithStatus(http.StatusNotFound)
-	}
-}
-
-// Joke contains information about a single Joke
-type Joke struct {
-	ID    int    `json:"id" binding:"required"`
-	Likes int    `json:"likes"`
-	Joke  string `json:"joke" binding:"required"`
-}
-
-// We'll create a list of jokes
-var jokes = []Joke{
-	Joke{1, 0, "Did you hear about the restaurant on the moon? Great food, no atmosphere."},
-	Joke{2, 0, "What do you call a fake noodle? An Impasta."},
-	Joke{3, 0, "How many apples grow on a tree? All of them."},
-	Joke{4, 0, "Want to hear a joke about paper? Nevermind it's tearable."},
-	Joke{5, 0, "I just watched a program about beavers. It was the best dam program I've ever seen."},
-	Joke{6, 0, "Why did the coffee file a police report? It got mugged."},
-	Joke{7, 0, "How does a penguin build it's house? Igloos it together."},
-}
-
 var userDB = make(map[string][]int)
 var jwtMiddleWare *jwtmiddleware.JWTMiddleware
 var log = logrus.New()
@@ -387,9 +345,6 @@ func main() {
 			})
 		})
 	}
-
-	api.GET("/jokes", JokeHandler)
-	api.POST("/jokes/like/:jokeID", LikeJoke)
 
 	api.GET("/localreps", LocalRepsHandler)
 	api.POST("/localreps/add", authMiddleware(), AddLocalRep)
